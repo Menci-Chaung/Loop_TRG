16b721e4fefe341dcfa250a237b567d6ea4bd8a8
Yue Zhengyuan <yuezy1997@icloud.com>
2018-11-26 09:17:47 +0800

    Initial Commit

---
+++ Loop_TRG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Loop_TRG
+//
+//  Created by Zhengyuan Yue on 2018/11/26.
+//  Copyright © 2018 Zhengyuan Yue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
